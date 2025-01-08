b83882c96a30695202cb3aefb6bb8981ae677e88
PinsaraPerera <1pawanpinsara@gmail.com>
2025-01-08 23:58:37 +0530

    testing script

--- test.py
+++ test.py
@@ -3,13 +3,14 @@ with open('Employee_Management_System/data.txt', 'r') as file:
     data = file.read()
     records = data.split('\n')
 
-print(records)
 
 employee_list = []
 
 for record in records:
-    employee_details = record.split(',')
-    employee_details = [_attr.strip() for _attr in employee_details]
-    _id, _name, _salary, _department = employee_details
-    
+    if record:
+        employee_details = record.split(',')
+        employee_details = [_attr.strip() for _attr in employee_details]
+        _id, _name, _salary, _department = employee_details
 
+        print(_id, _name, _salary, _department)
+        # print(employee_details)
